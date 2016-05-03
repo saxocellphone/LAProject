ad5310ee2ddc46ce66573fa25d475d85baea4304
Victor Nazzaro <victor741naruto@gmail.com>
2016-05-03 21:27:57 +0000

    asdf

--- js/linear_regression.js
+++ js/linear_regression.js
@@ -2,10 +2,13 @@ var width = 800;
 var height = 800;
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, width / height, 0.1, 1000 );
-
+var controls = new THREE.OrbitControls( camera );
+  
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(width, height);
 document.body.appendChild( renderer.domElement );
+
+controls.addEventListener( 'change', render );
 
 var geometry = new THREE.BoxGeometry( 1, 1, 1 );
 var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
@@ -18,17 +21,20 @@ var xBasis = new THREE.Vector3(1,0,0);
 var yBasis = new THREE.Vector3(0,1,0);
 var zBasis = new THREE.Vector3(0,0,1);
 
-var material = new THREE.LineBasicMaterial({
-	color: 0x0000ff,
-	linewidth: 5
-});
-
 var axisHelper = new THREE.AxisHelper(20);
-scene.add( axisHelper );
+scene.add(axisHelper);
 
-function render() {
-	requestAnimationFrame( render );
-	renderer.render( scene, camera );
+
+function animate()
+{
+	window.requestAnimationFrame(animate);
+	controls.update();
 }
-render();
 
+function render()
+{
+	renderer.render(scene, camera);
+}
+
+animate();
+render();
