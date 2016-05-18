0df5717302b0735df3824ac3525aefdd572794a8
Victor Nazzaro <victor741naruto@gmail.com>
2016-05-17 22:54:32 -0400

    Convert to node.js

--- js/image_processing.js
+++ js/image_processing.js
@@ -1,43 +1,17 @@
-(function () {
-    var takePicture = document.querySelector("#take-picture"),
-        showPicture = document.querySelector("#show-picture");
+function previewFile(){
+      var preview = document.querySelector('img'); //selects the query named img
+      var file    = document.querySelector('input[type=file]').files[0]; //sames as here
+      var reader  = new FileReader();
 
-    if (takePicture && showPicture) {
-        // Set events
-        takePicture.onchange = function (event) {
-            // Get a reference to the taken picture or chosen file
-            var files = event.target.files,
-                file;
-            if (files && files.length > 0) {
-                file = files[0];
-                try {
-                    // Create ObjectURL
-                    var imgURL = window.URL.createObjectURL(file);
+      reader.onloadend = function () {
+          preview.src = reader.result;
+      }
 
-                    // Set img src to ObjectURL
-                    showPicture.src = imgURL;
+      if (file) {
+          reader.readAsDataURL(file); //reads the data as a URL
+      } else {
+          preview.src = "";
+      }
+ }
 
-                    // Revoke ObjectURL
-                    URL.revokeObjectURL(imgURL);
-                }
-                catch (e) {
-                    try {
-                        // Fallback if createObjectURL is not supported
-                        var fileReader = new FileReader();
-                        fileReader.onload = function (event) {
-                            showPicture.src = event.target.result;
-                        };
-                        fileReader.readAsDataURL(file);
-                    }
-                    catch (e) {
-                        //
-                        var error = document.querySelector("#error");
-                        if (error) {
-                            error.innerHTML = "Neither createObjectURL or FileReader are supported";
-                        }
-                    }
-                }
-            }
-        };
-    }
-})();
+ previewFile();
